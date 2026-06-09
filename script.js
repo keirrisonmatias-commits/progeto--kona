ec605867fec824882c59769c55216b6068a14c2d
keirrisonmatias-commits <keirrison.matias@escola.pr.gov.br>
2026-06-09 17:46:57 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+/html><!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha Lista de Tarefas</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="todo-container">
+        <h2>Lista de Tarefas</h2>
+        
+        <div class="input-section">
+            <input type="text" id="taskInput" placeholder="Digite uma nova tarefa...">
+            <button id="addBtn">Adicionar</button>
+        </div>
+
+        <ul id="taskList">
+            </ul>
+    </div>
+
+    <script src="script.js"></script>
+</body>
